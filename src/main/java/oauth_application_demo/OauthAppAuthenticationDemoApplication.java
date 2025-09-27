ec07a2a3455a33989917a6f043f3c2c478d9246b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-27 10:56:42 +0530

    first application setup commit

---
+++ src/main/java/oauth_application_demo/OauthAppAuthenticationDemoApplication.java
@@ -0,0 +1,13 @@
+package oauth_application_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAppAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAppAuthenticationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oauth_application_demo/OauthAppAuthenticationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth_application_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAppAuthenticationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
